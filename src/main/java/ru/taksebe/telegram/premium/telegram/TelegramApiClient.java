5adb3b07a97cbca7ade35d0b3c38a9e41b224313
kozyrevsa <kozyrevsa@answer-42.ru>
2023-06-16 13:15:16 +0300

    Добавлена отправка инструкции (команда /start) как файла (идентификатор Telegram со временем забывает)

--- src/main/java/ru/taksebe/telegram/premium/telegram/TelegramApiClient.java
+++ src/main/java/ru/taksebe/telegram/premium/telegram/TelegramApiClient.java
@@ -12,10 +12,12 @@ import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.api.objects.ApiResponse;
 import ru.taksebe.telegram.premium.exceptions.TelegramFileNotFoundException;
+import ru.taksebe.telegram.premium.exceptions.TelegramFileUploadByIdException;
 import ru.taksebe.telegram.premium.exceptions.TelegramFileUploadException;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.Objects;
 
@@ -26,6 +28,7 @@ public class TelegramApiClient {
     String botToken;
 
     String startMessagePhotoFileId;
+    String startMessagePhotoFilePath;
     String startMessageText;
 
     String tempFileNamePrefix;
@@ -35,34 +38,23 @@ public class TelegramApiClient {
     public TelegramApiClient(@Value("${telegram.api-url}") String URL,
                              @Value("${telegram.bot-token}") String botToken,
                              @Value("${message.start.picture-file-id}") String startMessagePhotoFileId,
+                             @Value("${message.start.picture-file-path}") String startMessagePhotoFilePath,
                              @Value("${message.start.text}") String startMessageText,
                              @Value("${files.incoming}") String tempFileNamePrefix) {
         this.URL = URL;
         this.botToken = botToken;
         this.tempFileNamePrefix = tempFileNamePrefix;
         this.startMessagePhotoFileId = startMessagePhotoFileId;
+        this.startMessagePhotoFilePath = startMessagePhotoFilePath;
         this.startMessageText = startMessageText;
         this.restTemplate = new RestTemplate();
     }
 
-    public void uploadStartPhoto(String chatId) {
-        LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
-        map.add("photo", this.startMessagePhotoFileId);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
-
+    public void uploadStartPhoto(String chatId) throws IOException {
         try {
-           restTemplate.exchange(
-                    MessageFormat.format("{0}bot{1}/sendPhoto?chat_id={2}&caption={3}",
-                            URL, botToken, chatId, this.startMessageText),
-                    HttpMethod.POST,
-                    requestEntity,
-                    String.class);
-        } catch (Exception e) {
-            throw new TelegramFileUploadException();
+            uploadPhotoByFileId(chatId);
+        } catch (TelegramFileUploadByIdException e) {
+            uploadPhotoAsFile(chatId);
         }
     }
 
@@ -100,6 +92,59 @@ public class TelegramApiClient {
         } catch (Exception e) {
             throw new TelegramFileNotFoundException();
         }
+    }
+
+    private void uploadPhotoByFileId(String chatId) {
+        LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("photo", this.startMessagePhotoFileId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
+
+        try {
+            restTemplate.exchange(
+                    MessageFormat.format("{0}bot{1}/sendPhoto?chat_id={2}&caption={3}",
+                            URL, botToken, chatId, this.startMessageText),
+                    HttpMethod.POST,
+                    requestEntity,
+                    String.class);
+        } catch (Exception e) {
+            throw new TelegramFileUploadByIdException();
+        }
+    }
+
+    private void uploadPhotoAsFile(String chatId) throws IOException {
+        LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("photo", getPhotoResource());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
+
+        try {
+            restTemplate.exchange(
+                    MessageFormat.format("{0}bot{1}/sendPhoto?chat_id={2}", URL, botToken, chatId),
+                    HttpMethod.POST,
+                    requestEntity,
+                    String.class);
+        } catch (Exception e) {
+            throw new TelegramFileUploadException();
+        }
+    }
+
+    private ByteArrayResource getPhotoResource() throws IOException {
+        byte[] data = Objects.requireNonNull(this.getClass().getClassLoader()
+                .getResourceAsStream(this.startMessagePhotoFilePath)).readAllBytes();
+
+        return new ByteArrayResource(data) {
+            @Override
+            public String getFilename() {
+                return "start.jpg";
+            }
+        };
     }
 
     private String getVoiceTelegramFileUrl(String fileId) {
